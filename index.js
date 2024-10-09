68674959abe52d310ef4d047d971597ed24ff99b
Akshaykalhane <akshaykalhane1999@gmail.com>
2024-10-09 16:44:13 +0530

    minor changes

--- index.js
+++ index.js
@@ -40,12 +40,11 @@ app.post('/age-transform',async(req,res)=>{
     return
   }
 
-  
   const output = await replicate.run(
       "yuval-alaluf/sam:9222a21c181b707209ef12b5e0d7e94c994b58f01c7b2fec075d2e892362f13c",
       {
         input: {
-          image:image, //image
+          image:imagebase64, //image
           target_age:target_age
       }
     }
